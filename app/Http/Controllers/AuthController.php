392c721c2732227871fee8a2a55ef92fcd0748a1
Harris Sontanu <harris.sontanu@gmail.com>
2023-04-09 07:08:49 +0800

    Store new registered user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(): string
     {
         return "This is my login method";
     }
 
-    public function register()
+    public function register(StoreUserRequest $request)
     {
-        return response()->json('This is my register method');
+        $request->validated();
+
+        $user = User::create([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'password'  => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            'user'  => $user,
+            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken,
+        ]);
     }
 
     public function logout()
