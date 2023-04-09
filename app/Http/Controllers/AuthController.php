bf17650d7dea706b630ab8a7fd41993730ef7f78
Harris Sontanu <harris.sontanu@gmail.com>
2023-04-09 16:12:52 +0800

    Authentication login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(): string
+    public function login(LoginUserRequest $request)
     {
-        return "This is my login method";
+        $request->validated();
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'The credentials do not match.', 401);
+        }
+        
+        $user = User::whereEmail($request->email)->first();
+
+        return $this->success([
+            'user'  => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken,
+        ]);
     }
 
     public function register(StoreUserRequest $request)
